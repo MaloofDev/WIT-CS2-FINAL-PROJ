76b552e345d9cb929eb03c5d800829ba4610fb0c
maguiretatwit <144469528+maguiretatwit@users.noreply.github.com>
2023-11-18 12:27:07 -0500

    grabs team names and prints them in order

--- SportsDataScraper2/src/TestGrab.java
+++ SportsDataScraper2/src/TestGrab.java
@@ -11,13 +11,31 @@ public class TestGrab
 	
 	
 	public static void main(String[] args) throws IOException {
-	final String url = "https://www.espn.com/nba/stats/team";
+	/*final String url = "https://www.espn.com/nba/stats/team";
 	final Document doc = Jsoup.connect(url).get();
 	String html = doc.html();
 	html = html.substring(html.indexOf("teamStats"), html.indexOf("dictionary"));
 	System.out.println(html);
-	
+	for(int i=0;i<30;i++) {
+		html=html.substring(html.indexOf("displayName\":"));
+		System.out.println(html.substring(html.indexOf("\"")+3,html.indexOf("\",")));
+		html=html.substring(html.indexOf("short"));
+	}*/
 	//System.out.println(doc.select("img.Image.Logo.Logo__sm"));
+		
+		TeamGrab("https://www.espn.com/nba/stats/team",30);
+	}
+	public static void TeamGrab(String url, int teamSize) throws IOException {
+		final Document doc = Jsoup.connect(url).get();
+		String html = doc.html();
+		
+		//System.out.println(html);
+		html = html.substring(html.indexOf("teamStats"), html.indexOf("dictionary"));
+		for(int i=0;i<teamSize;i++) {
+			html=html.substring(html.indexOf("displayName\":"));
+			System.out.println(html.substring(html.indexOf("\"")+3,html.indexOf("\",")));
+			html=html.substring(html.indexOf("short"));
+		}
 	}
 
 }
