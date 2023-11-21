d4e37347e22bfc50a292b194565d7864983a2be8
maguiretatwit <144469528+maguiretatwit@users.noreply.github.com>
2023-11-21 09:30:34 -0500

    Add files via upload

--- SportsDataScraper2/src/ExpectedWinPercentage.java
+++ SportsDataScraper2/src/ExpectedWinPercentage.java
@@ -8,19 +8,9 @@ package expected_win_percentage;
  */
 public class ExpectedWinPercentage {
 	
-	private static double calculateExpectedWinPercentage(int pointsFor, int pointsAgainst) {
-		if (pointsFor == 0 || pointsAgainst == 0) {
-			return 0.0;
-		}
-		double expectedWinPercentage = Math.pow(pointsFor, 2)/ (Math.pow(pointsFor, 2) + Math.pow(pointsAgainst, 2));
+	public float calculateExpectedWinPercentage(float pointsFor, float pointsAgainst) {
+		float expectedWinPercentage = (float) ((float) (Math.pow(pointsFor, 2))/ (Math.pow(pointsFor, 2) + Math.pow(pointsAgainst, 2)));
 		return expectedWinPercentage;
-	}
-
-	public static void main(String[] args) {
-		int a = 772;
-		int b = 776;
-		System.out.printf("%.3f%n", calculateExpectedWinPercentage(a, b));
-
 	}
 
 }
---
+++ SportsDataScraper2/src/MLBStandings.java
@@ -0,0 +1,40 @@
+package expected_win_percentage;
+
+public class MLBStandings {
+	
+	private float W, L, PCT, RS, RA, xPCT;
+	
+	private String team;
+	
+	public MLBStandings(String name, float[] standings) {
+		this.team = name;
+		W = standings[0];
+		L = standings[1];
+		PCT = standings[2];
+		RS = standings[3];
+		RA = standings[4];
+		xPCT = standings[5];
+	}
+	public float getW() {
+		return W;
+	}
+	public float getL() {
+		return L;
+	}
+	public float getPCT() {
+		return PCT;
+	}
+	public float getRS() {
+		return RS;
+	}
+	public float getRA() {
+		return RA;
+	}
+	public float getxPCT() {
+		return xPCT;
+	}
+	public String getTeam() {
+		return team;
+	}
+
+}
