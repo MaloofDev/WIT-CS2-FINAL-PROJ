f5edef4b25fabca596555990e382895cd2d70c63
maguiretatwit <144469528+maguiretatwit@users.noreply.github.com>
2023-11-28 12:07:23 -0500

    Add files via upload

---
+++ SportsDataScraper2/src/NFLStandings.java
@@ -0,0 +1,40 @@
+package expected_win_percentage;
+
+public class NFLStandings {
+private float W, L, PCT, PF, PA, xPCT;
+	
+	private String team;
+	
+	public NFLStandings(String name, float[] standings) {
+		this.team = name;
+		W = standings[0];
+		L = standings[1];
+		PF = standings[2];
+		PA = standings[3];
+		PCT = standings[4];
+		xPCT = standings[5];
+	}
+	public float getW() {
+		return W;
+	}
+	public float getL() {
+		return L;
+	}
+	public float getPF() {
+		return PF;
+	}
+	public float getPA() {
+		return PA;
+	}
+	public float getPCT() {
+		return PCT;
+	}
+	public float getxPCT() {
+		return xPCT;
+	}
+	public String getTeam() {
+		return team;
+	}
+
+
+}
---
+++ SportsDataScraper2/src/NHLStandings.java
@@ -0,0 +1,40 @@
+package expected_win_percentage;
+
+public class NHLStandings {
+private float W, L, PCT, GF, GA, xPCT;
+	
+	private String team;
+	
+	public NHLStandings(String name, float[] standings) {
+		this.team = name;
+		W = standings[0];
+		L = standings[1];
+		GF = standings[2];
+		GA = standings[3];
+		PCT = standings[4];
+		xPCT = standings[5];
+	}
+	public float getW() {
+		return W;
+	}
+	public float getL() {
+		return L;
+	}
+	public float getGF() {
+		return GF;
+	}
+	public float getGA() {
+		return GA;
+	}
+	public float getPCT() {
+		return PCT;
+	}
+	public float getxPCT() {
+		return xPCT;
+	}
+	public String getTeam() {
+		return team;
+	}
+
+
+}
